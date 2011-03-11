6795a63ca8de0eebe8dbd1dc652f9bc05d81ddd8
Daniel Hillenbrand <info@codeworkx.de>
2011-03-11 03:28:15 -0800

    galaxysb.mk: disabled extended JNI checks

--- galaxysb.mk
+++ galaxysb.mk
@@ -139,6 +139,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
         ro.com.google.locationfeatures=1 \
         ro.com.google.networklocation=1
 
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
+
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
