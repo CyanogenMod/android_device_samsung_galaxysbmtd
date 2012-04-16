caa3899fe72ad40cf4211352227d30540e74aa8c
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-04-16 21:16:48 +0700

    galaxysbmtd: Use integrated kernel building
    Change-Id: Ide06ba222959f0b731cca74329cb0709f593a58a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,8 +20,8 @@
 #Video Devices
 BOARD_SECOND_CAMERA_DEVICE := /dev/video2
 
-#Prebuilt Kernel
-TARGET_PREBUILT_KERNEL := device/samsung/galaxysbmtd/kernel
+# Kernel Config
+TARGET_KERNEL_CONFIG := cyanogenmod_galaxysbmtd_defconfig
 
 # FM Radio
 BOARD_HAVE_FM_RADIO := true
